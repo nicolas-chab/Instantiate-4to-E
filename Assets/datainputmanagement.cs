ed235da174ad59fe2a59fdddc7cab79216346a93
nicolas-chab <101563616+nicolas-chab@users.noreply.github.com>
2022-05-10 15:47:50 -0300

    start

---
+++ Assets/datainputmanagement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class datainputmanagement : MonoBehaviour
+{
+    public Text txtUsername;
+    public void ShowGreetings()
+    {
+        Debug.Log("hola " + txtUsername);
+    }
+    public void ShowDouble()
+    {
+        if(txtUsername.text != "")
+        {
+            Debug.Log(int.);
+        }
+    }
+}
